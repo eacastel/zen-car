8508c6f41d32821ff91bdae5095417b1c686c2ac
eacastel <emilio@aliigo.com>
2025-04-25 13:02:38 -0700

    added facebook pixel

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -7,7 +7,7 @@
 // You can delete this file if you're not using it
 import * as React from "react"
 
-export const onRenderBody = ({ setHeadComponents, setHtmlAttributes }) => {
+export const onRenderBody = ({ setHeadComponents, setHtmlAttributes, setPostBodyComponents }) => {
   setHtmlAttributes({ lang: 'en' });
   setHeadComponents([
     <link
@@ -50,6 +50,34 @@ export const onRenderBody = ({ setHeadComponents, setHtmlAttributes }) => {
       crossOrigin="anonymous"
       key="HelveticaFont"
     />,
-
-  ])
+    <script
+      key="fbq-init"
+      defer
+      dangerouslySetInnerHTML={{
+        __html: `
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.defer=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1211874510591693');
+          fbq('track', 'PageView');
+        `,
+      }}
+    />,
+  ]);
+  setPostBodyComponents([
+    <noscript key="fbq-noscript">
+      <img
+        height="1"
+        width="1"
+        style={{ display: "none" }}
+        src="https://www.facebook.com/tr?id=1211874510591693&ev=PageView&noscript=1"
+        alt="Meta Pixel"
+      />
+    </noscript>,
+  ]);
 }
