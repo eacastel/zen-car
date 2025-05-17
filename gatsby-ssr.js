5eaa199ea401471301a5e76aeb72cecf6d7f6421
eacastel <emilio@aliigo.com>
2025-05-17 11:35:25 -0700

    set google tag in ssr.js head and prebody

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -7,7 +7,7 @@
 // You can delete this file if you're not using it
 import * as React from "react"
 
-export const onRenderBody = ({ setHeadComponents, setHtmlAttributes, setPostBodyComponents }) => {
+export const onRenderBody = ({ setHeadComponents, setHtmlAttributes, setPreBodyComponents, setPostBodyComponents }) => {
   setHtmlAttributes({ lang: 'en' });
   setHeadComponents([
     <link
@@ -66,6 +66,25 @@ export const onRenderBody = ({ setHeadComponents, setHtmlAttributes, setPostBody
           fbq('init', '1211874510591693');
           fbq('track', 'PageView');
         `,
+      }}
+    />,
+    <script
+      key="gtm-script"
+      dangerouslySetInnerHTML={{
+        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-W2LQ7S42');`,
+      }}
+    />,
+  ]);
+  setPreBodyComponents([
+    <noscript
+      key="gtm-noscript"
+      dangerouslySetInnerHTML={{
+        __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W2LQ7S42"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
       }}
     />,
   ]);
